cf6559d49270e5a92ac40329d29318c7338d7ccc
gssggssg <490104722@qq.com>
2023-02-25 01:01:49 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  extends: ['airbnb'],
+  parser: '@babel/eslint-parser',
+  plugins: ['react', 'jest', 'unicorn', 'react-hooks'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    mocha: true,
+    jest: true,
+    jasmine: true,
+  },
+  rules: {
+    strict: ['error', 'never'],
+    'react/display-name': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/state-in-constructor': 0,
+    'react/static-property-placement': 0,
+    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/no-array-index-key': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/require-default-props': 0,
+    'react/jsx-fragments': 0,
+    'react/jsx-wrap-multilines': 0,
+    'react/prop-types': 0,
+    'react/forbid-prop-types': 0,
+    'react/sort-comp': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'generator-star-spacing': 0,
+    'function-paren-newline': 0,
+    'sort-imports': 0,
+    'class-methods-use-this': 0,
+    'no-confusing-arrow': 0,
+    'linebreak-style': 0,
+    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
+    'no-prototype-builtins': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    // Conflict with prettier
+    'arrow-body-style': 0,
+    'arrow-parens': 0,
+    'object-curly-newline': 0,
+    'implicit-arrow-linebreak': 0,
+    'operator-linebreak': 0,
+    'no-param-reassign': 2,
+    'space-before-function-paren': 0,
+    'react/self-closing-comp': 1,
+    'react/jsx-key': 1,
+  },
+  settings: {
+    // support import modules from TypeScript files in JavaScript files
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+      },
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx', '.d.ts'],
+    'import/external-module-folders': ['node_modules', 'node_modules/@types'],
+    polyfills: ['fetch', 'Promise', 'URL', 'object-assign'],
+    react: {
+      version: 'detect',
+    },
+  },
+};
