fd46d5b90112c88d3d024c318f81228fdb4e2b8e
shenjiancheng <1311703897@qq.com>
2023-03-14 10:12:11 +0800

    feat: 添加备案号

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -4,7 +4,7 @@ import styles from '@/layout/layout.module.scss';
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
-      <div>备案号</div>
+      <div>滇ICP备2022007312号</div>
     </footer>
   );
 };
