6e2b061cf2a188aea3389c2f9d838aa60c099177
gssggssg <490104722@qq.com>
2023-03-17 23:56:31 +0800

    feat: Simple handling of ui-neumorphism components

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -3,12 +3,18 @@ import { Outlet } from 'react-router-dom';
 import Header from '@/layout/Header';
 import Footer from '@/layout/Footer';
 import styles from '@/layout/layout.module.scss';
+import { Card as NCard } from 'ui-neumorphism';
+
+const Card: any = NCard;
 
 const BaseLayout: React.FC = () => {
+  // @ts-ignore
   return (
     <main className={styles.main}>
       <Header />
-      <Outlet />
+      <Card inset>
+        <Outlet />
+      </Card>
       <Footer />
     </main>
   );
