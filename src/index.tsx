8fbdb13028fbac6a14ef254e70c2e0c1e79c431d
gssggssg <490104722@qq.com>
2023-03-27 22:32:02 +0800

    fix: Modify line breaks

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
