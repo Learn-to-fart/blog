637fd832cf13126bfe24ff4dba137a5c578d27bb
gssggssg <490104722@qq.com>
2023-03-29 23:36:04 +0800

    feat: Add markdown editor

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,55 +1,18 @@
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-// @ts-ignore
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-// @ts-ignore
-import { vscDarkPlus, coyWithoutShadows, darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
-
+import React, { useState } from 'react';
+import MdEditor from 'md-editor-rt';
+import 'md-editor-rt/lib/style.css';
 import styles from './index.module.scss';
 
 type tProps = {
   textContent: string;
-  language: string;
-  darkMode?: boolean;
-};
-
-const them = {
-  dark: vscDarkPlus,
-  light: coyWithoutShadows
 };
 
 const Markdown = (props: tProps) => {
-  const { textContent, darkMode } = props;
-  if (typeof darkMode === 'undefined') {
-    them.light = darcula;
-  }
-  if (typeof darkMode === 'boolean') {
-    them.light = coyWithoutShadows;
-  }
+  const { textContent } = props;
+  const [text, setText] = useState<string>(textContent);
   return (
     <div className={styles.container}>
-      <ReactMarkdown
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
-              <SyntaxHighlighter
-                showLineNumbers={true}
-                style={darkMode ? them.dark : them.light}
-                language={match[1]}
-                PreTag="div"
-                {...props}>
-                {String(children).replace(/\n$/, '')}
-              </SyntaxHighlighter>
-            ) : (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            );
-          }
-        }}>
-        {textContent}
-      </ReactMarkdown>
+      <MdEditor modelValue={text} onChange={setText} previewOnly={true} noPrettier={true} />
     </div>
   );
 };
