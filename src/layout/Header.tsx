b3ca45810fb86ee7c69a9477650b08622ca45e75
gssggssg <490104722@qq.com>
2023-09-25 22:37:44 +0800

    feat: Add a blog page

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import styles from '@/layout/layout.module.scss';
 
 const Header: React.FC = () => {
+  const menu: { name: string; path: string }[] = [
+    { name: '首页', path: '/home' },
+    { name: '博客', path: '/article' }
+  ];
   return (
     <header className={styles.header}>
       <div className={styles.headerLogo}>logo</div>
       <div className={styles.headerTitle}>
-        欢迎来到
-        <a href="http://shenjiancheng.com/" className={styles.headerTitleLink}>
-          shenjiancheng.com
-        </a>
+        {menu.map((item) => {
+          return (
+            <a
+              href={window.location.origin + item.path}
+              className={[styles.headerTitleLink, window.location.pathname.includes(item.path) ? styles.active : ''].join(' ')}
+              key={item.name}>
+              {item.name}
+            </a>
+          );
+        })}
       </div>
     </header>
   );
