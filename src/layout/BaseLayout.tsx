0221905a534b6d6d09582fe5aacb0ff3fd0a6a50
gssggssg <490104722@qq.com>
2023-11-05 22:28:44 +0800

    fix: upadta layout

--- src/layout/BaseLayout.tsx
+++ src/layout/BaseLayout.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '@/layout/Header';
 import Footer from '@/layout/Footer';
-import { Divider } from 'ui-neumorphism';
 import styles from '@/layout/layout.module.scss';
 
 const BaseLayout: React.FC = () => {
   return (
     <main className={styles.main}>
       <Header />
-      <Divider dense />
       <div className={styles.content}>
         <Outlet />
       </div>
